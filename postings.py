10b6b53d9c59cd058fb107f90e1da78f49ad2239
Mattia Setzu <setzu.mattia@gmail.com>
2024-12-11 18:15:10 +0100

    added postings 1

--- postings.py
+++ postings.py
@@ -14,3 +14,28 @@ def posting(sequence: Sequence) -> dict:
         posting_list[element] = posting_list.get(element, list()) + [index]
 
     return posting_list
+
+def sequence_pair(start, finish, sequence: Sequence) -> bool:
+    """La sequenza data inizia con `start` e finisce con `finish`?
+
+    :param start: Inizio sequenza
+    :param finish: Fine sequenza
+    :param sequence: La sequenza
+    :return: True se inizia con `start` e finisce con `finish`, False altrimenti.
+    """
+    posting_list = posting(sequence)
+
+    # non ho entrambi gli elementi: sicuramente False
+    if not(start in posting_list and finish in posting_list):
+        return False
+
+    # la condizione e' vera se per almeno un indice di start, trovo un indice di finish
+    # di valore maggiore!
+    for start_index in posting_list[start]:
+        if any([
+            finish_index > start_index
+            for finish_index in posting_list[finish]
+        ]):
+            return True
+
+    return False
